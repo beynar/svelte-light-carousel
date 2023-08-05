10b4560a71c56babc9f1a1dd24865fafea56728f
Beynar <21969002+beynar@users.noreply.github.com>
2023-08-05 19:06:25 +0200

    better partial view example

--- src/examples/PartialViewCarousel.svelte
+++ src/examples/PartialViewCarousel.svelte
@@ -12,6 +12,11 @@
 	import { slides } from './slides.js';
 </script>
 
-<Carousel {slides} layout={{ sm: 1, default: 2 }} partialDelta={{ default: 100 }}>
+<Carousel
+	{slides}
+	layout={{ sm: 1, default: 2 }}
+	gaps={{ xs: 5, sm: 10, default: 20 }}
+	partialDelta={{ xs: 20, sm: 40, md: 60, lg: 80, xl: 100 }}
+>
 	<Slide slot="slide" let:slide {...slide} />
 </Carousel>
